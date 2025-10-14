bbe753b10b8f16867ea3305db3115ca6c6a1d45f
Trinh Tran <ttran@markany.com>
2025-10-14 14:52:47 +0700

    fix: change date format to MM/DD/YYYY for subscription period

--- packages/utils/dist/index.mjs
+++ packages/utils/dist/index.mjs
@@ -11278,8 +11278,8 @@ var formatDateTimeLong = (date) => {
   try {
     return new Intl.DateTimeFormat("en-US", {
       year: "numeric",
-      month: "long",
-      day: "numeric",
+      month: "2-digit",
+      day: "2-digit",
       hour: "numeric",
       minute: "2-digit",
       hour12: true
