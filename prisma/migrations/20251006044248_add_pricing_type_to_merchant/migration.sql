8a88999ac62bfb947d42c10c564452f0a6298cfe
Trinh Tran <ttran@markany.com>
2025-10-17 22:22:12 +0700

    feat: add isDefault field to Category model
    - Add isDefault boolean field to Category schema
    - Set default value to false
    - Prepare for protecting default categories from deletion
    - Similar to outlet isDefault protection pattern

--- prisma/migrations/20251006044248_add_pricing_type_to_merchant/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Merchant" ADD COLUMN "pricingConfig" TEXT DEFAULT '{"businessType":"GENERAL","defaultPricingType":"FIXED","businessRules":{"requireRentalDates":false,"showPricingOptions":false},"durationLimits":{"minDuration":1,"maxDuration":1,"defaultDuration":1}}';
-ALTER TABLE "Merchant" ADD COLUMN "pricingType" TEXT;
