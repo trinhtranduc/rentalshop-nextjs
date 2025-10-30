f49cf893c5643740b422d39f4fd80872218b03ba
Trinh Tran <ttran@markany.com>
2025-10-30 13:55:42 +0700

    chore(prisma): remove all migrations

--- prisma/migrations/20251017223643_add_isdefault_to_category/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Category" ADD COLUMN "isDefault" BOOLEAN NOT NULL DEFAULT false;
-
--- Update existing "General" categories to be default
-UPDATE "Category" SET "isDefault" = true WHERE "name" = 'General';
-
