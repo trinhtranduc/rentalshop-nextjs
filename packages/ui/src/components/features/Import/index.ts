59ffa4b7ad47b6cd9135b77071da94e05776d1eb
Trinh Tran <trinhduc20@gmail.com>
2026-01-09 18:25:58 +0700

    fix(ui): fix component exports for Products and Customers features
    - Fix Products component export: change from default export re-export to named export
    - Fix Customers component export: change from default export re-export to named export
    - Add ImportProductDialog export in Products components index
    - Add ImportCustomerDialog export in Customers components index
    - Add CSVPreviewTable export in Import feature index
    
    This fixes 'Element type is invalid' errors in ProductsPage and CustomersPage

--- packages/ui/src/components/features/Import/index.ts
+++ packages/ui/src/components/features/Import/index.ts
@@ -5,8 +5,10 @@
 export { FileUploadZone } from './FileUploadZone';
 export { ImportInstructions } from './ImportInstructions';
 export { ImportPreviewTable } from './ImportPreviewTable';
+export { CSVPreviewTable } from './CSVPreviewTable';
 
 export type { FileUploadZoneProps } from './FileUploadZone';
 export type { ImportInstructionsProps } from './ImportInstructions';
 export type { ImportPreviewTableProps } from './ImportPreviewTable';
+export type { CSVPreviewTableProps } from './CSVPreviewTable';
 
